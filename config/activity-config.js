d1bc208d8301fa677f7623f4c2723163f67900c5
gauravsinghIG <gaurav.singh@impactguru.com>
2025-05-27 14:52:23 +0530

    Project Initialized

---
+++ config/activity-config.js
@@ -0,0 +1,29 @@
+const ActivityConfig = [
+    {
+        activityType: 'LINKEDIN_POST',
+        activityIcon: 'linkedin',
+        activityColor: '#0077B5',
+        activityTitle: 'Did you know what happens when you run `git git push origin @`?',
+        activityLink: 'https://www.linkedin.com/posts/gauravsngh_git-gittips-developertools-activity-7330056604970577920-dsWQ',
+        activityDescription: '',
+        activityTime: '2023-05-20T12:00:00Z'
+    },
+    {
+        activityType: 'LINKEDIN_POST',
+        activityIcon: 'linkedin',
+        activityColor: '#1DA1F2',
+        activityTitle: 'Over-communication is always better than under-communication',
+        activityLink: 'https://www.linkedin.com/posts/gauravsngh_communication-leadership-productmanagement-activity-7328984276736086016-UL_q',
+        activityDescription: '',
+        activityTime: '2023-05-18T12:00:00Z'
+    },
+    {
+        activityType: 'LINKEDIN_POST',
+        activityIcon: 'linkedin',
+        activityColor: '#1DA1F2',
+        activityTitle: "It's not my bug",
+        activityLink: 'https://www.linkedin.com/posts/gauravsngh_qualityassurance-ownership-leadership-activity-7328257550645149696-UCHI',
+        activityDescription: '',
+        activityTime: '2023-05-16T12:00:00Z'
+    },
+];
